9337ea0413ff5df59441a13502ecc0da29581634
Roy-Tsai-myaccount <55198239+Roy-Tsai-myaccount@users.noreply.github.com>
2021-03-21 12:39:02 +0800

    Add files via upload

---
+++ SystemC/basics/Hello World/hello.cpp
@@ -0,0 +1,17 @@
+/********************************
+
+Title: Hello World
+Originated from (http://www.asic-world.com/systemc/first1.html)
+Author: Roy Tsai
+Date: Mar. 19, 2021
+
+********************************/
+//My first program in SystemC.
+
+#include "hello.h"
+
+void hello_world::hello_proc(){ //Process
+		
+		std::cout << "Hello World.\n" << endl;
+		
+	}
